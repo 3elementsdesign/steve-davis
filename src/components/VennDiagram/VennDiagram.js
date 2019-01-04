45d655950f5038a930a5c31d2a22441a4e4e38c3
3elementsdesign <44347268+3elementsdesign@users.noreply.github.com>
2019-01-03 20:31:56 -0800

    adding animation

--- src/components/VennDiagram/VennDiagram.js
+++ src/components/VennDiagram/VennDiagram.js
@@ -2,18 +2,39 @@ import React from "react";
 
 import "./VennDiagram.scss";
 
-const VennDiagram = props => (
-  <div className="c-venn">
-    <div className="c-venn__circle c-venn__circle--ux-designer">
-      <span className="c-venn__name">UX Designer</span>
-    </div>
-    <div className="c-venn__circle c-venn__circle--ux-engineer">
-      <span className="c-venn__name">UX Engineer</span>
-    </div>
-    <div className="c-venn__circle c-venn__circle--js-engineer">
-      <span className="c-venn__name">Javascript Engineer</span>
-    </div>
-  </div>
-);
+class VennDiagram extends React.Component {
+  state = {
+    visible: true
+  };
+  handleClick = () => {
+    this.setState({
+      visible: !this.state.visible
+    });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <div
+          className={this.state.visible ? "c-venn" : "c-venn c-venn--hidden"}
+        >
+          <div className="c-venn__circle c-venn__circle--ux-designer">
+            <span className="c-venn__name">UX Designer</span>
+          </div>
+          <div className="c-venn__circle c-venn__circle--ux-engineer">
+            <span className="c-venn__name">UX Engineer</span>
+          </div>
+          <div className="c-venn__circle c-venn__circle--js-engineer">
+            <span className="c-venn__name">Javascript Engineer</span>
+          </div>
+        </div>
+
+        <button className="c-toggle-venn" onClick={this.handleClick}>
+          Toggle
+        </button>
+      </React.Fragment>
+    );
+  }
+}
 
 export default VennDiagram;
